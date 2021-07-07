735eb96db243c4e665e3b2350af326ea4573699c
Hasan Saleh <71003887+hsnsl@users.noreply.github.com>
2021-07-07 12:29:27 +0300

    Learn C++

---
+++ learncpp/simpleinfo.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+  string color,hobby,food;
+  cout << "enter a color:";getline(cin,color);cout<<"enter a hobby:";getline(cin,hobby);cout<<"enter a food:";getline(cin,food);cout << "your favourite color is  "<<color<<", your favourite is hobby is "<<hobby<<" , your favourite food is  "<<food<<endl;
+  return 0;
+ }
+  
