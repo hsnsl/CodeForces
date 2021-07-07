f4a85f3fc5b1d650afc921031a7dab5b28000794
Hasan Saleh <71003887+hsnsl@users.noreply.github.com>
2021-07-07 12:18:12 +0300

    Learn Cpp

---
+++ learncpp/numbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h> // include math library
+#include<cmath> // include second math library
+using namespace std;
+int main(){
+    cout << 5+5<<endl;
+    cout << 4*3<<endl;
+    cout << 12/4 << endl;
+    cout << 5-10<<endl;
+    cout << 5.5 + 5.1<<endl;
+    cout << sqrt(64)<<endl;
+    cout << hypot(3,4)<<endl;//sqrt(3*3+4*4) pythagores
+    cout << pow(2,5) << endl;// 2^5=32
+    cout <<  round(2.6)<<endl;//round 2.6 = 3
+    cout << ceil(2.1) <<endl;//2+1=3
+    cout << floor(2.8)<<endl;//2
+  return 0;
+}
