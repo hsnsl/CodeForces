dae0160df1acd1259bf6fdcc911e8589406eab5d
Hasan Saleh <71003887+hsnsl@users.noreply.github.com>
2021-07-07 13:47:15 +0300

    Learn C++

---
+++ learncpp/pointers.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int age=9;
+    cout << &age<<endl;//out the physical address of variable 0x999ef example
+  return 0;
+}
