c2f3a96657f3d28ec91d7ebefc0ac6a9e8228d3c
Hasan Saleh <71003887+hsnsl@users.noreply.github.com>
2021-07-07 12:20:23 +0300

    Learn C++

---
+++ learncpp/inputs.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;int age;
+    cin>>name>>age;
+    cout << "your name is " << name << " , you have "<<age<<" years old"<<endl;
+  return 0;
+}
