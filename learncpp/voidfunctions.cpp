bd3617dff5b83aeb356feffb938f206d76295308
Hasan Saleh <71003887+hsnsl@users.noreply.github.com>
2021-07-07 12:36:20 +0300

    Learn C++

---
+++ learncpp/voidfunctions.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void sh(string name,int age){
+    cout << "Hello,"<<name<<", you have "<<age<<" years old"<<endl;
+}
+int main(){
+    sh("hasan",14);
+  return 0;
+ }
+  
