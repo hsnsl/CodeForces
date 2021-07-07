706069f207d51906b1579e9d3bd83caf9ef2d9ee
Hasan Saleh <71003887+hsnsl@users.noreply.github.com>
2021-07-07 11:47:39 +0300

    Learn C++

---
+++ learncpp/var1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+  string name="hasan";
+  int age=14;
+  float school=97.2;
+  char lovedletter = 'h';
+  cout << "My Name is "+name+" , I'm "<<age<<" years old,"<<" my school average is "<<school<<"my loved letter is "<<lovedletter<<endl;
+  return 0;
+}
