70bdd84d26beb8633a64718d65f8be48fc4056cc
Hasan Saleh <71003887+hsnsl@users.noreply.github.com>
2021-07-07 11:41:24 +0300

    Learn C++

---
+++ learncpp/drawtriangle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    cout << "   / |"<<endl;
+    cout << "  /  |"<<endl;
+    cout <<" /   |"<<endl;
+    cout <<"/____|"<<endl;
+  return 0;
+}
